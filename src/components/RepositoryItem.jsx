daf5959c8dd5fb4c3130317f82349511fd3a8531
Facundo Benitez <facu.cr9@gmail.com>
2023-03-05 18:58:42 -0300

    staring the columns with flexbox

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,21 +2,43 @@ import React from 'react'
 import { View, Text, StyleSheet } from "react-native";
 import StyledText from './StyledText';
 
-const RepositoryItem = (props) => (
-        <View
-            key={props.id}
-            style={styles.container}
-        >
+const RepositoryStats = props => {
+    return (
+        <View style={{flexDirection: 'row', justifyContent:'space-around'}}>
 
-            <StyledText fontSize='subheading' fontWeight='bold'>{props.fullName}</StyledText>
-            <StyledText >{props.description}</StyledText>
-            <StyledText >Language: {props.language}</StyledText>
-            <StyledText >Stars: {props.stargazersCount}</StyledText>
-            <StyledText >Forks: {props.forksCount}</StyledText>
-            <StyledText >Review: {props.reviewCount}</StyledText>
-            <StyledText >Rating: {props.ratingAverage}</StyledText>
+            <View>
+                <StyledText fontWeight='bold'>Stars</StyledText>
+                <StyledText>{props.stargazersCount}</StyledText>
+            </View>
+            <View>
+                <StyledText fontWeight='bold'>Forks</StyledText>
+                <StyledText>{props.forksCount}</StyledText>
+            </View>
+            <View>
+                <StyledText fontWeight='bold'>Review</StyledText>
+                <StyledText>{props.reviewCount}</StyledText>
+            </View>
+            <View>
+                <StyledText fontWeight='bold'>Rating</StyledText>
+                <StyledText>{props.ratingAverage}</StyledText>
+            </View>
 
         </View>
+    )
+}
+const RepositoryItem = (props) => (
+    <View
+        key={props.id}
+        style={styles.container}
+    >
+
+        <StyledText fontSize='subheading' fontWeight='bold'>{props.fullName}</StyledText>
+        <StyledText >{props.description}</StyledText>
+        <StyledText >Language: {props.language}</StyledText>
+
+        <RepositoryStats {...props} />
+
+    </View>
 );
 export default RepositoryItem;
 
